d0919ee6de9f3ae15ffb45a798f76fecc1bceae6
Sudhakar Sah <sudhakarsah@Sudhakars-MacBook-Air.local>
2025-08-21 22:42:26 +0545

    first coomit

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs =require('fs');
+
+// const name= "John Deo";
+// const des="is decent guy";
+// const age=25;
+// console.log(name,des, ", his age is:", age);
+
+const neee = fs.readFileSync('./txt/dot.txt' ,'utf-8');
+console.log(neee);
+
+
+
+
+
